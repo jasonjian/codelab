2df590f80fe687f378072cfd77cac556c4665499
jasonj <jjian@advaoptical.com>
2020-01-13 17:55:12 +0800

    add cpplab to test cpp

---
+++ base/cpplab/smartptr/sharedcount.hpp
@@ -0,0 +1,18 @@
+class SharedCount{
+public:
+    long getCount() noexcept
+    {
+        return 
+    }
+    long decreaseCount() noexcept
+    {
+
+    }
+    long increaseCount() noexcept
+    {
+
+    }
+
+private:
+    long mCount;
+};
